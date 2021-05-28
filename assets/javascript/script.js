2db386cc125fb47927543a6e38459a963daa0a6b
Megan Steblay <megan@steblay.net>
2021-05-27 22:26:06 -0500

    tweaking JS file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -646,16 +646,16 @@ hideInit();
 
 
 // LOCATION BUTTON FUNCTION (previous)
-let buttonContainer = $("#locationButtons");
+// let buttonContainer = $("#locationButtons");
 
-function goToLocation(event) {
-    let object = $(event.target)
-    if (object.is( ":button" )) {
-        window.location.href = `${object["0"].id}.html`
-    } else {
-        window.location.href = `${object.parent()["0"].id}.html`
-    }
+// function goToLocation(event) {
+//     let object = $(event.target)
+//     if (object.is( ":button" )) {
+//         window.location.href = `${object["0"].id}.html`
+//     } else {
+//         window.location.href = `${object.parent()["0"].id}.html`
+//     }
 
-}
+// }
 
-buttonContainer.on("click", "button", goToLocation)
+// buttonContainer.on("click", "button", goToLocation)
