764b31ee531ce6e57a5b2265fe736e0120136317
Heather Bjoin <h.m.bjoin@gmail.com>
2021-05-27 20:45:29 -0500

    gettin' the stuff looking pretty

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,12 +2,11 @@ let buttonContainer = $("#locationButtons");
 
 function goToLocation(event) {
     let object = $(event.target)
-    if (object.is( ":button" )) {
+    if (object.is(":button")) {
         window.location.href = `${object["0"].id}.html`
     } else {
         window.location.href = `${object.parent()["0"].id}.html`
     }
 
 }
-
 buttonContainer.on("click", "button", goToLocation)
