f91f6cef4aab4da6c327faaf02d8b01e79c1cc39
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-05-28 09:50:18 -0500

    adding preload copyright screen to index

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,21 @@
 // ********************************* VARIABLES ************************************ //
+// preload index screen
+$(document).ready(function(){
+    $("#copyrightBtn").click(function(){
+        $("#removeScreen").fadeToggle(2000);
+    });
+});
+// $(document).ready(function() {
+	
+// 	setTimeout(function(){
+// 		$('body').addClass('loaded');
+// 		$('h1').css('color','#222222');
+// 	}, 3000);
+	
+// });
+
+
+let buttonContainer = $("#locationButtons");
 
 let background = document.querySelector('#backgroundImage');
 let audioEl = document.querySelector('#audio');
